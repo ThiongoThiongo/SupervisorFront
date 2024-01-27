f6869a00aa4a14ecd8d6d7ceb90a4080fb97f572
ThiongoThiongo <thiongothiongo753@gmail.com>
2024-01-26 23:15:12 -0800

    final

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       port:3003,
       proxy:{
         '/api': {
-          target:'http://localhost:5000',
+          target:'https://instacartbackend.onrender.com',
           changeOrigin:true
         }
     
